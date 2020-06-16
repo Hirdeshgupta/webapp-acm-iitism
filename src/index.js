0e88bdadf9fb04c0261e63eb5dcae10390c14759
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-06-16 06:16:34 -0700

    About added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom"
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "mdbreact/dist/css/mdb.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import App from "./App"
