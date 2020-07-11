b682aafff95c153c1814b3072a0ddf35545b9025
Yash vardhan <yashvardhan513@gmail.com>
2020-07-11 17:29:35 +0530

    update pre-loaders

--- src/components/Events/EventsDetails.js
+++ src/components/Events/EventsDetails.js
@@ -3,6 +3,10 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 import { Link } from "react-router-dom";
 import { MDBBtn,MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import ReactLoading from "react-loading";
+import "bootstrap/dist/css/bootstrap.css";
 var storageRef = firebase.storage().ref();
 
 class EventsDetails extends React.Component{
@@ -13,6 +17,7 @@ class EventsDetails extends React.Component{
 
       this.state={
           EVENTS:[],
+          done: undefined
        }
 
   }
@@ -32,16 +37,25 @@ class EventsDetails extends React.Component{
 
 
         this.setState({
-          EVENTS: previousEvents
+          EVENTS: previousEvents,
         })
       })
+
+      setTimeout(() => {
+       fetch("https://jsonplaceholder.typicode.com/posts")
+         .then(response => response.json())
+         .then(json => this.setState({ done: true }));
+     }, 4000);
 
   }
 
 
 render() {
+
   return (
     <div>
+
+    (
       <br></br>
       <br></br>
       <br></br>
@@ -51,6 +65,10 @@ render() {
           <MDBCard>
             <MDBCardImage className="img-fluid" id="new-img" waves />
               {
+                !this.state.done ? (
+                 <ReactLoading type={"bars"} color={"black"}  className="loading-events"/>
+                ) : (
+
                     this.state.EVENTS.map((eve,index) => {
                         if(eve.id===this.props.location.state.id){
                       let spaceRef = storageRef.child('IMAGES/'+this.state.EVENTS[index].imageURL)
@@ -58,9 +76,9 @@ render() {
                           document.getElementById('new-img').src = url;
                     })
                     }
-                  }
-
+                  })
               )
+
                 }
 
             <MDBCardBody className="txt_body">
@@ -71,10 +89,10 @@ render() {
           </MDBCard>
         <MDBBtn outline color="primary" className="backToEvents"><Link style={{ fontSize: "19px", padding: "7px" }} to={'/events'}>Back to Events List</Link></MDBBtn>
         </MDBCol>
-    </div> 
+
+    </div>
   )
 }
 }
 
 export default EventsDetails
-
