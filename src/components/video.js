72964f7b360cfacd4ffd3a540bdf56036fcd68b2
Aakash01ak <aksingh01ak@gmail.com>
2020-07-15 12:18:19 +0530

    update slider

--- src/components/video.js
+++ src/components/video.js
@@ -3,11 +3,9 @@ import React from 'react'
 function Video(){
     return(
         <div style={{marginTop:"0"}}>
-<video width="100%" height="100%" autoPlay muted>
-  <poster src={require('./logo.png')}/>
+<video width="100%" height="100%" autoPlay muted poster={require("./abc.jpg")}>
   <source src={require("./img/vid.mp4")} type="video/mp4"/>
-  {/* <source src={require("./img/vid.ogg")} type="video/ogg"/> */}
-{/* Your browser does not support the video tag. */}
+ Your browser does not support the video tag
 </video>
         </div>
 
