d15e4d2419641f4bba66550f115b35b0ec5def2c
Aakash01ak <aksingh01ak@gmail.com>
2020-07-28 16:06:14 +0530

    update join button

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Button } from 'reactstrap';
+import React, { Fragment } from "react";
+import { MDBBtn } from "mdbreact";
 
 function benefits(){
     return(
+        
         <div style={{margin:"20px 0"}}>
         <img className="bene" src={require("./benefits-02.png")}></img>
-        <Button color="primary" className="join" size="lg"  href="https://forms.gle/D2VcWQbNTDRpNwsr6">Join Now</Button>
+        <Fragment>
+        <MDBBtn gradient="peach" className="join" size="lg" href="https://forms.gle/D2VcWQbNTDRpNwsr6">Join Now</MDBBtn>
+      </Fragment>
         </div>
     )
 }
