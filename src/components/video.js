337d6b994ac4c4b183b5a6a4b90b5149a92eb54a
Aakash01ak <aksingh01ak@gmail.com>
2020-07-30 11:39:31 +0530

    video changed

--- src/components/video.js
+++ src/components/video.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Video(){
     return(
         <div style={{marginTop:"0"}}>
-<video width="100%" height="100%" autoPlay muted poster={require("./abc.jpg")} preLoad="metadata">
+<video width="100%" height="100%" autoPlay muted loop poster={require("./abc.JPG")} preLoad="metadata">
   <source src={require("./img/vid.mp4")} type="video/mp4"/>
  Your browser does not support the video tag
 </video>
