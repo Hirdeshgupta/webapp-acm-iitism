5266148ba85a668e452a837ccaec3cd5dbf33153
Hirdeshgupta <hirdeshgupta16@gmail.com>
2020-07-31 23:55:50 +0530

    Benefits image compressed

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -6,7 +6,7 @@ function benefits(){
         
         <div style={{margin:"20px 0"}}>
         <MDBAnimation reveal type="fadeIn">
-        <img className="bene" src={require("./benefits-02.png")}></img>
+        <img className="bene" src={require("./benefits-02.jpg")}></img>
         </MDBAnimation>
         <Fragment>
         <MDBBtn gradient="peach" className="join" size="lg" href="https://forms.gle/D2VcWQbNTDRpNwsr6">Join Now</MDBBtn>
