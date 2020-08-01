f45923d1159a6e5340f68ec629a810c35d71baa8
Yash vardhan <yashvardhan513@gmail.com>
2020-08-01 22:16:24 +0530

    update benefits page

--- src/components/benefits.js
+++ src/components/benefits.js
@@ -3,10 +3,12 @@ import { MDBBtn } from "mdbreact";
 import {MDBAnimation} from "mdbreact"
 function benefits(){
     return(
-        
+
         <div style={{margin:"20px 0"}} id="benefit">
         <MDBAnimation reveal type="fadeIn">
+        <MDBAnimation reveal type="zoomIn">
         <img className="bene" src={require("./benefits-02.jpg")}></img>
+        </MDBAnimation>
         </MDBAnimation>
         <Fragment>
         <MDBBtn gradient="peach" className="join" size="lg" href="https://forms.gle/D2VcWQbNTDRpNwsr6">Join Now</MDBBtn>
@@ -14,5 +16,6 @@ function benefits(){
         </div>
     )
 }
+
 
 export default benefits;
