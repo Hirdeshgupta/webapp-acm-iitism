582cb6840efb30a5b9afb15112456f529e36eaf3
Yash vardhan <yashvardhan513@gmail.com>
2020-08-04 17:54:41 +0530

    update typewriter.js

--- src/components/typewriter.js
+++ src/components/typewriter.js
@@ -24,7 +24,7 @@ class typewriter extends React.Component {
         <MDBRow>
           <MDBCol className="z-depth-0">
             <MDBJumbotron style={{ padding: 0 }}>
-              <MDBCol className="text-white text-center px-0 " style={{ height: this.state.height, backgroundImage: `url(https://mdbootstrap.com/img/Photos/Others/gradient1.jpg)` }}>
+              <MDBCol className="text-white typewriter-col text-center px-0 " style={{ height: this.state.height,  }}>
                 <MDBCol className="py-5 px-0">
                   <MDBCardTitle className="h1-responsive pt-5 my-5 font-bold text-white" style={{fontFamily:"FF Tisa" ,fontSize:"2.5rem",padding:"0 35px 0px 35px"}}><Typewriter
                     options={{
