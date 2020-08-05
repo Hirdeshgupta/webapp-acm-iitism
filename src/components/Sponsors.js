789d476ba9ede1455a9cc6f0409aceacfc63f9a6
Aakash01ak <aksingh01ak@gmail.com>
2020-08-05 08:20:53 +0530

    rearrange and add link to sponsors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -121,201 +121,226 @@ class Sponsors extends React.Component{
           </MDBAnimation>
           <div className="container z-depth-0 mt-0 pt-2 mb-4 px-0 pr-4" style={{background:"white"}}>
           <div className="row justify-content-around  px-md-5 my-md-5">
-          <MDBView hover>
+          <a href="https://www.samsung.com/in/" target="_blank">
+            <MDBView hover>
           <MDBAnimation reveal type="fadeIn">
             <div className="col-md-6  my-2 my-md-1">
-              <img src={require("./img/ff/codechef.png")} className="my-3" alt=""/>
+              <img src={require("./img/ff/5.png")} className="my-3" alt="Samsung"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
             </MDBView>
+            </a> 
+            <a href="http://www.walmart.com/" target="_blank" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
             <div className="col-md-6 my-2 my-md-1">
-              <img src={require("./img/ff/codingblocks.png")} className="my-3" alt=""/>
+              <img src={require("./img/ff/7.png")} className="my-3" alt="Walmart"/>
             </div>
             <MDBMask overlay="white-light " />
             </MDBAnimation>
             </MDBView>
+            </a>
             </div>
             <div className="row justify-content-around  px-md-5 ">
+            <a href="https://www.mozilla.org/en-US/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/codingninjas.png")} className="my-3" alt=""/>
+              <img src={require("./img/ff/4.png")} className="my-3" alt="Mozilla"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+            <a href="https://github.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/devfolio.png")} className="my-3" alt=""/>
+             <img src={require("./img/ff/github.png")} className="my-3" alt="Github"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
-
+            </a> 
             </div>
             <div className="row justify-content-around  px-md-5 my-md-5">
+            <a href="https://www.codechef.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/foxmula.png")} className="my-3" alt=""/>
+              <img src={require("./img/ff/codechef.png")} className="my-3" alt="Codechef"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+             <a href="https://www.geeksforgeeks.org/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/geeksforgeeks.png")} className="my-3" alt=""/>
+             <img src={require("./img/ff/geeksforgeeks.png")} className="my-3" alt="Geeksforgeeks"/>
             <MDBMask  overlay="white-light " />
 
             </div>
             </MDBAnimation>
 
             </MDBView>
-
+            </a>
             </div>
             
             <div className="row justify-content-around px-md-5 my-md-5">
+            <a href="https://www.hackerrank.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 mr-4  my-md-1">
-              <img src={require("./img/ff/github.png")}  className="my-3" alt=""/>
+              <img src={require("./img/ff/1.png")}  className="my-3" alt="Hackerrank"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+            <a href="https://www.hackerearth.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 ml-4 my-md-1">
-              <img src={require("./img/ff/skillenzatransparent.png")} className="my-3" alt=""/>
+             <img src={require("./img/ff/2.png")} className="my-3" alt="Hackerearth"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
-
+            </a> 
             </div>
           
           {/* ========= */}
           <div className="row justify-content-around  px-md-5 my-md-5">
+          <a href="https://codingblocks.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/techgig.png")} className="my-3" alt=""/>
+             <img src={require("./img/ff/codingblocks.png")} className="my-3" alt="Coding Blocks"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+            <a href="https://www.codingninjas.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/1.png")} className="my-3"  alt=""/>
+              <img src={require("./img/ff/codingninjas.png")} className="my-3"  alt="Coding Ninjas"/>
             <MDBMask  overlay="white-light " />
 
             </div>
             </MDBAnimation>
 
             </MDBView>
-
+            </a>
             </div>
 
 
 
             <div className="row justify-content-around  px-md-5 my-md-5">
+            <a href="https://skillenza.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/2.png")} className="my-3"  alt=""/>
+              <img src={require("./img/ff/skillenzatransparent.png")} className="my-3"  alt="Skillenza"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+            <a href="https://www.vedantalimited.com/Pages/Home.aspx" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/3.png")} className="my-3"  alt=""/>
+              <img src={require("./img/ff/3.png")} className="my-3"  alt="Vedanta"/>
             <MDBMask  overlay="white-light " />
 
             </div>
             </MDBAnimation>
 
             </MDBView>
-
+            </a>
             </div>
 
 
 
             
             <div className="row justify-content-around  px-md-5 my-md-5">
+            <a href="https://devfolio.co/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/4.png")} className="my-3"  alt=""/>
+              <img src={require("./img/ff/devfolio.png")} className="my-3"  alt="Devfolio"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+            <a href="https://foxmula.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/5.png")} className="my-3"  alt=""/>
+             <img src={require("./img/ff/foxmula.png")} className="my-3"  alt="Foxmula"/>
             <MDBMask  overlay="white-light " />
 
             </div>
             </MDBAnimation>
 
             </MDBView>
-
+            </a>
             </div>
 
 
             
             <div className="row justify-content-around  px-md-5 my-md-5">
+            <a href="https://www.teqip.in/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/6.png")} className="my-3"  alt=""/>
+             <img src={require("./img/ff/6.png")} className="my-3"  alt="Teqip"/>
             </div>
             <MDBMask  overlay="white-light " />
             </MDBAnimation>
 
             </MDBView>
+            </a>
+            <a href="https://www.techgig.com/" target="_blank">
             <MDBView hover>
             <MDBAnimation reveal type="fadeIn">
 
             <div className="col-lg-6 my-2 my-md-1">
-              <img src={require("./img/ff/7.png")} className="my-3"  alt=""/>
+              <img src={require("./img/ff/techgig.png")} className="my-3"  alt="Techgig"/>
             <MDBMask  overlay="white-light " />
 
             </div>
             </MDBAnimation>
 
             </MDBView>
-
+            </a>
             </div>
           </div>
             
